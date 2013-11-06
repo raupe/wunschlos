d2699e39d47b51073af0a5e686c2209380200725
Duc Ngo Viet <d.ngoviet@gmail.com>
2013-11-06 22:46:50 +0100

    updated testclient to test local api development

--- server/testclient/main.js
+++ server/testclient/main.js
@@ -1,20 +1,75 @@
 //var url = "http://localhost:3000/wishlist";
 var url = "http://place2co.de/nodejs/wishlist/wishlist"
 
+/*Route: /wishlist
+HTTP Verb: POST*/
 var request = $.ajax({
 	url: url,
 	type: "post",
 	data: {
-		name: "Birthday Party"
+		title: "Weihnachten",
+		to: "Andre",
+		items: [
+			{
+				id: 0,
+				title: "Power Ranger",
+				price: 20.0,
+				unit: "€",
+				link: "http://www.amazon.de/Power-Rangers-Der-Film-Turbo/dp/B00DRYZC3Q",
+				idea: "Duc",
+				position: 0,
+				share: [
+					{
+						id: 0,
+						name: "Jenny",
+						amount: 10.0
+    				}
+				],
+				comments: [
+					{
+						id: 0,
+						name: "Jenny",
+						comment: "Coole Sache"
+    				}
+				]
+  			}
+		]
 	}
 });
 
-request.done(function(msg){
-	
+/*Route: /wishlist
+HTTP Verb: GET*/
+//var request = $.ajax({
+//	url: url,
+//	type: "get",
+//	data: {
+//		id: '527ab38f3c570bb421000004'
+//	}
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+request.done(function (msg) {
+
 	console.log(msg);
 });
 
-request.fail(function(jqXHR, textStatus){
-	
+request.fail(function (jqXHR, textStatus) {
+
 	console.log("failed: " + textStatus);
 });
