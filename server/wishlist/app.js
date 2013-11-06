1de51c8445ba22f024e30a34da75e0c23a68e27f
Duc Ngo Viet <d.ngoviet@gmail.com>
2013-11-06 22:47:49 +0100

    restructure code

--- server/wishlist/app.js
+++ server/wishlist/app.js
@@ -1,24 +1,26 @@
-// load modules
+// Load modules
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
 
-// load own modules
+// Load own modules
 var routes = require('./routes');
+var schemas = require('./schemas');
 
-// create express app
+// Create express app
 var app = express();
 
 // Code to allow Cross Origin
 // Source: http://stackoverflow.com/questions/18642828/origin-http-localhost3000-is-not-allowed-by-access-control-allow-origin
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*"); // later only allow from wunsch-los.com
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+var allowCrossDomain = function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', "*"); // later only allow from wunsch-los.com
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 }
 
-// app configurations
+// App Configuration and middleware
 app.set('port', 3000);
 app.use(express.urlencoded()); // use that instead of bodyParser() because bodyParser is deprecated
 app.use(express.methodOverride()); // to use app.delete and app.put for better semantic than using post all the time: http://stackoverflow.com/questions/8378338/what-does-connect-js-methodoverride-do
@@ -27,23 +29,38 @@ app.use(express.session()); // to use sessions
 app.use(allowCrossDomain); // For CORS, use function declared above
 // app.use(app.router); // not really sure what that do, so outcommented
 
-// http://localhost:3000/hi
-// http://place2co.de/nodejs/wishlist/hi
-// Testroutes
+// Database
+mongoose.connect('mongodb://test:testpw@localhost:20883/test'); // online
+//mongoose.connect('mongodb://localhost:27017/test'); // local
+var Schema = mongoose.Schema;
+var connection = mongoose.connection;
+
+// define database schemas
+var wishlistSchema = new Schema(schemas.wishlist);
+
+// define models build from defined schemas
+var Wishlist = mongoose.model('Wishlist', wishlistSchema);
+
+// Routes http://localhost:3000/hi and http://place2co.de/nodejs/wishlist/hi
+// Testroute
 app.get('/hi', function (req, res) {
-	console.log(req.query.name);
+	//console.log(req.query.name);
 	res.send("get successfully");
-});
-app.post('/hi', function (req, res) {
-	console.log(req.body.name);
-	res.send("post successfully");
 });
 
 // Routes
-app.post("/wishlist", routes.createWishlist);
+app.post('/wishlist', routes.createWishlist(Wishlist));
+app.get('/wishlist', routes.getWishlist(Wishlist));
 
+connection.once('error', function() {
+	console.log("error: failed to connect to mongodb");
+});
 
-// Start the server
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+connection.once('open', function callback() {
+	console.log("DB Connection to mongodb opened");
+	
+	// Start the server
+	http.createServer(app).listen(app.get('port'), function () {
+		console.log('Express server listening on port ' + app.get('port'));
+	});
 });
