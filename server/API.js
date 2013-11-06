af8f48858df00fc1b1e0479fc7ef184305f49247
Duc Ngo Viet <d.ngoviet@gmail.com>
2013-11-06 22:46:04 +0100

    creating api doc

---
+++ server/API.js
@@ -0,0 +1,53 @@
+/*
+Route: /wishlist
+HTTP Verb: POST
+
+Beschreibung: Erstellen einer Wunschliste
+
+Rückgabe vom Server: id der Wunschliste
+Request zum Server: Wishlist als JSON siehe folgende Struktur "var wishlist"
+*/
+{
+	title: String,
+	to: String,
+	items: [
+		{
+			id: number,
+			title: String,
+			price: number,
+			unit: String, // €, $, piece (Stück)
+			link: String,
+			idea: String,
+			position: number,
+			share: [
+				{
+					id: number,
+					name: String,
+					amount: number
+    			}
+			],
+			comments: [
+				{
+					id: number,
+					name: String,
+					comment: String
+    			}
+			]
+  		}
+	]
+}
+
+/*
+Route: /wishlist
+HTTP Verb: GET
+
+Beschreibung: Anfordern einer Wunschliste
+Zum testen über Adresszeile:
+http://place2co.de/nodejs/wishlist/wishlist?id=527ab38f3c570bb421000004
+
+Rückgabe vom Server: die angeforderte Wunschliste
+Request zum Server: Id der Wunschliste als JSON
+*/
+{
+	id: String
+}
