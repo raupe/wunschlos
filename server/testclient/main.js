33e67f94f6e6be77ce6441c96e22b4417a30d86c
Duc Ngo Viet <d.ngoviet@gmail.com>
2013-11-11 00:20:23 +0100

    restructure test code

--- server/testclient/main.js
+++ server/testclient/main.js
@@ -1,75 +1,79 @@
-//var url = "http://localhost:3000/wishlist";
-var url = "http://place2co.de/nodejs/wishlist/wishlist"
+var url = "http://localhost:3000/wishlist";
+//var url = "http://place2co.de/nodejs/wishlist/wishlist"
 
-/*Route: /wishlist
-HTTP Verb: POST*/
-var request = $.ajax({
-	url: url,
-	type: "post",
-	data: {
-		title: "Weihnachten",
-		to: "Andre",
-		items: [
-			{
-				id: 0,
-				title: "Power Ranger",
-				price: 20.0,
-				unit: "€",
-				link: "http://www.amazon.de/Power-Rangers-Der-Film-Turbo/dp/B00DRYZC3Q",
-				idea: "Duc",
-				position: 0,
-				share: [
-					{
-						id: 0,
-						name: "Jenny",
-						amount: 10.0
-    				}
-				],
-				comments: [
-					{
-						id: 0,
-						name: "Jenny",
-						comment: "Coole Sache"
-    				}
-				]
-  			}
-		]
-	}
-});
-
-/*Route: /wishlist
-HTTP Verb: GET*/
-//var request = $.ajax({
-//	url: url,
-//	type: "get",
-//	data: {
-//		id: '527ab38f3c570bb421000004'
-//	}
+//var foo = document.createElement("button");
+//foo.innerHTML = "foo";
+//foo.addEventListener("click", function(){
+//	
 //});
+//document.body.appendChild(foo);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-request.done(function (msg) {
-
-	console.log(msg);
+var button = document.createElement("button");
+button.innerHTML = "POST /wishlist";
+button.addEventListener("click", function () {
+	/*Route: /wishlist
+	HTTP Verb: POST*/
+	var request = $.ajax({
+		url: url,
+		type: "post",
+		data: {
+			title: "Weihnachten",
+			to: "Andre",
+			items: [
+				{
+					id: 0,
+					title: "Power Ranger",
+					price: 20.0,
+					unit: "€",
+					link: "http://www.amazon.de/Power-Rangers-Der-Film-Turbo/dp/B00DRYZC3Q",
+					idea: "Duc",
+					position: 0,
+					share: [
+						{
+							id: 0,
+							name: "Jenny",
+							amount: 10.0
+    				}
+					],
+					comments: [
+						{
+							id: 0,
+							name: "Jenny",
+							comment: "Coole Sache"
+    				}
+					]
+  			}
+			]
+		}
+	});
+	request.done(function (msg) {
+		console.log(msg);
+	});
+	
+	request.fail(function (jqXHR, textStatus) {
+		console.log("failed: " + textStatus);
+	});
 });
+document.body.appendChild(button);
 
-request.fail(function (jqXHR, textStatus) {
-
-	console.log("failed: " + textStatus);
+var button2 = document.createElement("button");
+var id = "528012524523a3601c000010";
+button2.innerHTML = "GET /wishlist/" + id;
+button2.addEventListener("click", function(){
+	/*Route: /wishlist
+	HTTP Verb: GET*/
+	var request = $.ajax({
+		url: url + "/" + id,
+		type: "get"
+	});
+	request.done(function (msg) {
+		console.log(msg);
+	});
+	
+	request.fail(function (jqXHR, textStatus) {
+		console.log("failed: " + textStatus);
+	});
 });
+document.body.appendChild(button2);
+
+
