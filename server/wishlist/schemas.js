9927621d64bcd9530394daa4c1664a12d49e6038
Duc Ngo Viet <d.ngoviet@gmail.com>
2013-11-13 00:03:36 +0100

    remove id since it is created automatically, added secret and publicId

--- server/wishlist/schemas.js
+++ server/wishlist/schemas.js
@@ -1,27 +1,28 @@
 exports.wishlist = {
 	title: String,
 	to: String,
+	publicId: String, // wanted to use ObjectId here but throws error
 	items: [
 		{
-			id: Number,
 			title: String,
 			price: Number,
 			unit: String,
 			link: String,
 			idea: String,
 			position: 0,
+			secret: Boolean,
 			share: [
 				{
-					id: Number,
 					name: String,
-					amount: Number
+					amount: Number,
+					secret: Boolean
     			}
 			],
 			comments: [
 				{
-					id: Number,
 					name: String,
-					comment: String
+					comment: String,
+					secret: Boolean
     			}
 			]
   		}
