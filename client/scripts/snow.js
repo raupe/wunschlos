af42810c332b94eb8f9b6f327a81c369f0a931b6
lingfa <fabian@neurron.com>
2013-12-08 13:54:11 +0100

    optimze responsive snow

--- client/scripts/snow.js
+++ client/scripts/snow.js
@@ -1,98 +1,152 @@
-$(document).ready(function(){    
+/**
+ *  snow
+ *  ====
+ *
+ * Responsive snow animation and creation of a background-image.
+ * 
+ *	snow.init(); // to init snow animation and create background-image
+ *                // currently inited in the last code line
+ *  snow.stop();  // to stop animation and remove background-image
+ */
 
-	var contentHeight;
-	var contentWidth;
-	var snowflakeMargin = 150;
-	var SnowflakesCounter = 0;
-	var slowSpeed = 4;
-	var maxPeak = 10;
-	var fastSpeed = 10;
-	var minimunFlake = 3;
-	var smallFlake = 5;
-	var bigFlake = 8;
-	var snowStorage = {};
-	var designDiv = $("#design");
+$(document).ready(function(){ 
+	
+	var $bg_image = null; // background image
+	var bg_source = "url(assets/img/xmas.png)"; // source for background image
 	
 
+	snow = {
+
+		framerate: 20,
+		flakeMinSize: 3,
+		flakeMaxSize: 8,
+		flakesMargin: 75,
+		sinFrequency: 1,
+		maxSinusPeak: 0.3,
+		minSpeed: 1,
+		maxSpeed: 2,
+		canvas: null,
+		flakes: [],
+
+		init: function() {
+			createBackgroundImage();
+			snow.canvas = document.createElement('canvas');
+			snow.canvas.width = $(window).width();
+			snow.canvas.height = $(window).height();
+			snow.canvas.id = 'snowCanvas';
+			document.body.appendChild(snow.canvas);
+			snow.createFlakeShape();
+			snow.ctx = snow.canvas.getContext('2d'),
+			snow.canvas.style.position = 'fixed';
+			snow.canvas.style.bottom = 0;
+			snow.canvas.style.left = 0;
+			snow.canvas.style.zIndex = -1;			
+			window.snowtimer = window.setInterval(snow.draw, 1000/snow.framerate);
+			window.addEventListener('resize',snow.resize);
+			snow.flakeAmount();
+			snow.start();
+		},		
+		start: function() {
+			var c = snow.count;
+			snow.flakes = [];
+			do {
+				snow.flakes.push(new snow.flake());
+			} while(--c);
+		},
+		stop: function() {
+			window.clearInterval(window.snowtimer);
+			var canvas = document.getElementById('snowCanvas');
+			canvas.parentNode.removeChild(canvas);
+			window.snowtimer = snow = null;
+			$bg_image.remove();
+		},		
+		draw: function() {
+			var ctx = snow.ctx, snowFlakes = snow.flakes, snowCount = snow.count;
+			ctx.clearRect(0,0,snow.canvas.width,snow.canvas.height);
+
+			do {
+				if(snowFlakes[--snowCount].draw(ctx)) { };
+			} while(snowCount);
+		},
+		flake: function() {
+			this.draw = function(ctx) {
+			
+				ctx.drawImage(snow.flakeShape,this.x,this.y,this.size,this.size);		
+				ctx.fill();
+				this.animate();
+			};
+			this.animate = function() {
+				this.y += this.speed;
+				this.x += this.sinFrequency * Math.sin(snow.maxSinusPeak * snow.maxSinusPeak * this.y);
+				if(this.y > innerHeight)
+					this.init("resetY");
+			};
+			this.init = function(resetY) {
+				this.speed = snow.minSpeed + (Math.random() * (snow.maxSpeed - snow.minSpeed));
+				this.size = Math.floor(Math.random()*(snow.flakeMaxSize-snow.flakeMinSize+1)+snow.flakeMinSize);
+				this.sinFrequency = Math.floor((Math.random()*snow.sinFrequency)+1);
+				this.x = (Math.random() * snow.canvas.width ) - this.size;
+				this.y = resetY ? -this.size : Math.random() * snow.canvas.height;
+			};
+			this.init();
+		},
+		createFlakeShape: function() {
+			snow.flakeShape = document.createElement('canvas');
+			snow.flakeShape.width = snow.flakeShape.height = snow.flakeMaxSize;
+			var ctx = snow.flakeShape.getContext('2d');
+			ctx.fillStyle = 'rgba(255,255,255,0.7)';
+			ctx.arc(snow.flakeMaxSize/2,snow.flakeMaxSize/2,snow.flakeMaxSize/2,0,2*Math.PI);
+			ctx.fill();	
+		},
+		flakeAmount: function() {
+			var yRaster, xRaster, h, i;
+			yRaster = Math.floor(snow.canvas.height/snow.flakesMargin);
+			xRaster = Math.floor(snow.canvas.width/snow.flakesMargin);	
+			snow.count = 0;
+				// snow according to parent div dimension
+			for(h = 0; h < yRaster; h++){
+			
+				for(i =0; i < xRaster; i++){
+			
+					snow.count++
+				}
+			}				
+		},
+		resize: function() {
+			snow.canvas.width = $(window).width();
+			snow.canvas.height = $(window).height();
+			snow.flakeAmount();
+			snow.start();
+			positionBackgroundImage();
+		}		
+	};
+
 	
-	var snowflake = function Snowflake(x, y, id, speed, peak, flakeSize) {
-	
-		this.id = id;
-		this.x = x;
-		this.y = y;
-		this.speed = speed;
-		this.peak = peak;
-		this.flakeSize = flakeSize;
-	}
-	
-	function init(){
-	
-		contentHeight =  designDiv.height();
-		contentWidth = designDiv.width();
-		generateInitSnow();
-		
+			// creates background image 
+	var createBackgroundImage = function(){		
+
 		$('body').prepend(		
 
-			$('<img>') // new image
-				.attr('src', "assets/img/xmas.png")
+			$('<div>') 
+				.css('background-image', bg_source)
+				.css('background-position', "center bottom")
+				.css('background-repeat', "no-repeat")
 				.attr("id","bg_image")
-				.css('position', 'fixed')
+				.css('position', 'absolute')
+				.css('bottom', 0)
 				.css('z-index', "-1")
-				.css('width', 'auto')
-				.css('max-width', 'none')
+				.css('width', '100%')
+				.css('height', '100%')
 			
 			);
-			
-			positionBackgroundImage();
+		$bg_image =	$("#bg_image");
+		positionBackgroundImage();
 	}
- 
-	
-	//if height of wishlist grows, more snowflakes
-	/*designDiv.click(function(){
-		designDiv.css("height", 300);
-			designDiv.trigger('change');
-	});*/
- 
-		// if wish list grows/shrinks, more/less snowflakes are needed
-	designDiv.bind('change', function() {
-
-		contentHeight =  designDiv.height();
-		contentWidth = designDiv.width();
-		
-		var yRaster =Math.floor(contentHeight/snowflakeMargin);
-		var xRaster =Math.floor(contentWidth/snowflakeMargin);
-		var snowflakesAmount = yRaster * xRaster;
-		
-		// reduce or extend amount of snowflakes according to parent div
-		if(snowflakesAmount < SnowflakesCounter){
-		
-			var reduceIndex = SnowflakesCounter - snowflakesAmount;
-			var counter = 0;
-			
-			for (key in snowStorage) {
-				delete snowStorage[key];
-				$("#"+snowStorage[key].id).remove();
-				counter++;
-				SnowflakesCounter--;
-				if(counter === SnowflakesCounter) return;
-			}			
-		}
-		
-		if(snowflakesAmount > SnowflakesCounter){
-		
-			for(var i =0; i < xRaster; i++){
-		
-				createSnowflake(i); 
-			}			
-		}
-		
-	});
 
 		// change background position according to scroll	
 	$(document).scroll(function () {
 
 		positionBackgroundImage();
-
 	});
 
 		// ensure that snowman stands on the ground
@@ -104,148 +158,12 @@ $(document).ready(function(){
 			if(scrollAmount > footerPos){
 			
 				var footerPosCurrent = ($(window).height() - (footerPos - $(window).scrollTop()));
-				$("#bg_image").css("bottom", footerPosCurrent);							
+				$bg_image.css("bottom", footerPosCurrent-($(window).scrollTop()));				
+			
 			}else{
-				$("#bg_image").css("bottom", "auto");
+				$bg_image.css("bottom", -($(window).scrollTop()));
 			}		
 	}
-	
-	$( window ).resize(function() {
-	
-		if(this.resizeTO){
-						
-			clearTimeout(this.resizeTO);
-		}
-		
-		this.resizeTO = setTimeout(function() {
-		
-			$(this).trigger('resizeEnd');
-		}, 300);
-						
-		contentHeight =  designDiv.height();
-		contentWidth = designDiv.width();
 
-	});
-
-	$(window).bind('resizeEnd', function() {
-
-					for(var key in snowStorage) {
-
-			$("#"+snowStorage[key].id).remove();
-		}
-		generateInitSnow();
-		positionBackgroundImage();
-	});
-	
-	function generateInitSnow() {
-		
-		var yRaster = Math.floor(contentHeight/snowflakeMargin);
-		var xRaster = Math.floor(contentWidth/snowflakeMargin);
-		var h;
-		var i;
-	
-		// snow according to parent div dimension
-		for(h = 0; h < yRaster; h++){
-		
-			for(i =0; i < xRaster; i++){
-		
-				createSnowflake(i);
-			}
-		}	
-	}
-
-	function createSnowflake(count){
-	
-		// randomize the top position of the snow
-		var snowTop = Math.floor(Math.random()* contentHeight );		
-		
-		// randomize the left position of the snow
-		var snowLeft = Math.floor(Math.random()* contentWidth  );	
-		
-		var newId = "flake_"+SnowflakesCounter;
-		
-		var speed, peak, flakeSize;
-		
-		if( count % 2 == 0){
-
-			speed = slowSpeed;
-			peak = Math.floor((Math.random()*maxPeak)+1); 
-			flakeSize = Math.floor(Math.random()*(bigFlake-smallFlake+1)+smallFlake);
-
-		}else{
-			speed = fastSpeed;
-			peak = Math.floor((Math.random()*maxPeak)+1); 
-			flakeSize = Math.floor(Math.random()*(smallFlake-minimunFlake+1)+minimunFlake);
-		}
-		
-		snowStorage[SnowflakesCounter] = new snowflake(snowLeft, snowTop, newId, speed, peak, flakeSize);
-
-		designDiv.prepend(			
-				$('<div />')
-					.addClass('snow')
-					.css('width', flakeSize)
-					.css('height', flakeSize)
-					.css('top', snowTop)
-					.css('left', snowLeft)
-					.css('position','absolute')
-					.attr("id",newId)
-		);
-		
-		SnowflakesCounter++;		
-	}
-	
-    function snowFalling(){
-	
-		var x_current;
-		var y_current;
-		var id_flake;
-		var counter;
-		var speed;
-		var peak;
-		var flakeSize;
-		var x; 
-		var y; 
-		var flake_current;
-		
-		for(var key in snowStorage) {
-
-			x_current = snowStorage[key].x;
-			y_current = snowStorage[key].y;
-			id_flake = snowStorage[key].id;
-			speed = snowStorage[key].speed;
-			peak = snowStorage[key].peak;
-			flakeSize = snowStorage[key].flakeSize;
-			flake_current = $("#"+id_flake);
-			
-			x = Math.sin((y_current+1)/peak)*peak + x_current;
-			y = y_current + speed;
-
-			// check if the snow has reached the bottom of the parent
-			if( y > contentHeight - flakeSize ) {
-				x = Math.floor(Math.random()* contentWidth );
-				y = 0;
-				snowStorage[key].x = x;
-				snowStorage[key].y = y;				
-				flake_current.css({'top': y,'left': x}).fadeIn();		
-				
-			}else{
-			
-				snowStorage[key].x = x;
-				snowStorage[key].y = y;
-				flake_current.css({top: y+ "px", left: x + "px"});
-			}
-			
-			if((x > contentWidth - flakeSize) | (x < 0) ){
-				flake_current.hide()
-			}else{
-				flake_current.show();
-			}				
-
-	}
-		window.setTimeout(snowFalling, 160); 		
-		
-    }        
-
-    init();
-	snowFalling(); 
+	snow.init();
 });
